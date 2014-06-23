c0863d3be8d55d87304e0bfaa2ba8a0f449ba470
thbourlove <thbourlove@gmail.com>
2014-06-23 18:25:41 +0800

    add service provider

---
+++ src/Provider/Silex/ZeusServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace Eleme\Zeus\Provider\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Eleme\Zeus\Zeus;
+
+class ZeusServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['zeus'] = $app->share(function ($app) {
+            return new Zeus();
+        });
+    }
+
+    public function boot(Application $app)
+    {
+        Zeus::setClients(function () use ($app) {
+            return $app['thrift.clients'];
+        });
+        Zeus::setTimer(function () use ($app) {
+            return $app['timer']['zeus'];
+        });
+        Zeus::setCacher(function () use ($app) {
+            return $app['cacher'];
+        });
+    }
+}
